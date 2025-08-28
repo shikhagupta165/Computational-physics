a7c416a7d2df113fe0ade58c7387bf7677f2bb6b
shikhagupta165 <shikha.gupta@niser.ac.in>
2025-08-28 15:33:48 +0530

    Add files via upload

--- mylib.py
+++ mylib.py
@@ -1,3 +1,14 @@
+def read_matrix(filename):
+    with open(filename, 'r') as f:
+        print(f"file data {f.read()}")
+        matrix = []
+        for line in f:
+            print(f"file data {line}")
+            print(line)
+            row = [float(num) for num in line.strip().split()]
+            matrix.append(row)
+    return matrix
+
 #LCG
 def lcg(N=500, x0=0.1, coeff=4):
 
@@ -62,3 +73,53 @@ def gauss_jordan_solve(A, b, eps=1e-14):
 
     return [M[i][-1] for i in range(n)]
 
+#cholesky iteration
+def cholesky_decomposition(A):
+    n = len(A)
+    L = [[0.0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(i+1):
+            s = sum(L[i][k]*L[j][k] for k in range(j))
+            if i == j:
+                L[i][j] = (A[i][i] - s) ** 0.5
+            else:
+                L[i][j] = (A[i][j] - s) / L[j][j]
+    return L
+
+def forward_substitution(L, b):
+    n = len(L)
+    y = [0.0]*n
+    for i in range(n):
+        s = sum(L[i][j]*y[j] for j in range(i))
+        y[i] = (b[i] - s)/L[i][i]
+    return y
+
+def backward_substitution(L, y):
+    n = len(L)
+    x = [0.0]*n
+    for i in range(n-1, -1, -1):
+        s = sum(L[j][i]*x[j] for j in range(i+1, n))
+        x[i] = (y[i] - s)/L[i][i]
+    return x
+
+#jacobi iteration
+def jacobi_iteration(A, b, tol=1e-6, max_iter=1000):
+    n = len(b)
+    x = [0.0] * n
+    x_new = [0.0] * n
+
+    for iteration in range(max_iter):
+        for i in range(n):
+            s = 0.0
+            for j in range(n):
+                if j != i:
+                    s += A[i][j] * x[j]
+            x_new[i] = (b[i] - s) / A[i][i]
+
+        diff = max(abs(x_new[i] - x[i]) for i in range(n))
+        if diff < tol:
+            return x_new, iteration + 1
+
+        x = x_new[:]
+
+    return x, max_iter
